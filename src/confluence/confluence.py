f1eb547d6bb93b7329b90cbb748160d32df651ef
Branden Kappes <bkappes@mines.edu>
2019-12-19 13:39:38 -0700

    Initial rebasing to confluence.py to unify the command line interface.

--- src/confluence/confluence.py
+++ src/confluence/confluence.py
@@ -1,67 +1,33 @@
+"""
+Confluence provides a set of tools for joining, merging,
+listing, and manipulating Excel-formatted metadata files.
+The goal is to use a familiar tool (Excel) to structure/
+organize the collection of data and metadata that is flexible
+enough to adapt to a wide-range of projects.
+"""
+
 import sys
 import argparse
-from .list_items import list_items
-from .pif_maker import convert
 from .merge import run as merge_main
 from .merge import generate_merge_args
 from .list_items import list_items as list_main
 from .list_items import generate_list_args
 
-# class CLIparser():
-#     def __init__(self):
-#         self.parser = None
-#         self.set_arguments()
-#
-#     def set_arguments(self):
-#         self.parser = argparse.ArgumentParser(description='parse arguments')
-#         subparsers = self.parser.add_subparsers(help='subparser help')
-#
-#         merge_parser = subparsers.add_parser('merge', help='merge help')
-#         merge_parser.add_argument('infiles', nargs='*', help='input file name with no file type')
-#         merge_parser.add_argument('-o', '--output', help='output file name')
-#         merge_parser.add_argument('-i', '--input', nargs=2, help='input file name with accompanying file type', action='append')
-#         merge_parser.add_argument('-m', '--mergedefault', help='default solution to merge conflicts')
-#         merge_parser.add_argument('--interactive', action='store_true', help='make the program prompt user for input in case of conflict')
-#         merge_parser.add_argument('-s', '--sheetname', help='Specify a default sheetname in case writing to an xlsx file')
-#         merge_parser.add_argument('--outputformat', help='specify output file type')
-#         merge_parser.add_argument('-q', '--quiet', action='store_true', help='Should a merge conflict happen, default to abort')
-#         merge_parser.add_argument('-k', '--key', help='Specify the name of the smaple name column', default='Sample Name')
-# s
-#         list_parser = subparsers.add_parser('list', help='list help')
-#         list_parser.add_argument('action', help ='specifies action')
-#         list_parser.add_argument('-i', '--input', nargs=2, help='input file name with accompanying file type', action='append')
-#         list_parser.add_argument('infiles', nargs='*', help='input file name with no file type')
-#         list_parser.add_argument('-k', '--key', help='Specify the name of the smaple name column', default='Sample Name')
-#         list_parser.set_defaults(func=list_items)
-#
-#         pif_parser = subparsers.add_parser('makePIF', help='PIF help')
-#         pif_parser.add_argument('input_file', help='specifies file to convert to PIF')
-#         pif_parser.set_defaults(func=convert)
-#
-#
-#     def parse_args(self):
-#             args = self.parser.parse_args(sys.argv[1:])
-#             return args
-#
-#
-# def main():
-#     args = CLIparser().parse_args()
-#
-#
-# if __name__ == '__main__':
-#     main()
-#
 
-################################################################################################
-################################################################################################
+from confluence import __version__
+
+__author__ = "Branden Kappes"
+__copyright__ = "Branden Kappes"
+__license__ = "mit"
+
+_logger = logging.getLogger(__name__)
+
 
 
 def parse_args(args):
 
     if isinstance(args, list):
-        parser = argparse.ArgumentParser(
-        description="Comand Line Tool for Confluence",
-        help = "Comand Line Tool for Confluence")
+        parser = argparse.ArgumentParser(description=__doc__)
     else:
         parser = args
 
@@ -72,28 +38,47 @@ def parse_args(args):
 
     #Merge Parser
     merge_parser = subparsers.add_parser('merge', help='merge help')
-    merge_parser.set_defaults(func = merge_main)
+    merge_parser.set_defaults(func=merge_main)
     generate_merge_args(merge_parser)
 
     #List Parser
     list_parser = subparsers.add_parser('list', help='list help')
-    list_parser.set_defaults(func = list_main)
+    list_parser.set_defaults(func=list_main)
     generate_list_args(list_parser)
     return parser
 
 
-def main():
-    args = sys.argv[1:]
-    if isinstance(args,list):
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(level=loglevel, stream=sys.stdout,
+                        format=logformat, datefmt="%Y-%m-%d %H:%M:%S")
+
+
+def main(args):
+    """Main entry point allowing external calls
+
+    Args:
+      args ([str]): command line parameter list
+    """
+    if isinstance(args, list):
         args = parse_args(args)
+    setup_logging(args.loglevel)
+    _logger.debug("Starting calculation from climath...")
+    args.func(args)
+    _logger.info("End climath")
 
 
-if __name__ == '__main__':
-    main()
+def run():
+    """Entry point for console_scripts
+    """
+    main(sys.argv[1:])
 
 
-
-##main calls logging,
-
-################################################################################################
-################################################################################################
+if __name__ == "__main__":
+    run()
